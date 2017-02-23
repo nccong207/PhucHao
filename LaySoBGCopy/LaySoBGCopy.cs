0c39b25a09b3b5816a5e75dce07af00cabaf5f7c
linh093 <linh0893@gmail.com>
2017-02-23 23:01:42 +0700

    fix task 3

---
+++ LaySoBGCopy/LaySoBGCopy.cs
@@ -0,0 +1,62 @@
+﻿using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Grid;
+using FormFactory;
+using Plugins;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace LaySoBGCopy
+{
+    public class LaySoBGCopy: ICControl
+    {
+        private DataCustomFormControl _data;
+        private InfoCustomControl _info = new InfoCustomControl(IDataType.MasterDetailDt);
+        string oldBG = "";
+        public void AddEvent()
+        {
+            DataSet dsData = _data.BsMain.DataSource as DataSet;
+            
+            if (dsData == null)
+                return;
+            dsData.Tables[0].TableNewRow += new DataTableNewRowEventHandler(BaoGia_TableNewRow);
+            dsData.Tables[0].RowChanged += BaoGia_RowChanged;
+
+        }
+
+        void BaoGia_RowChanged(object sender, DataRowChangeEventArgs e)
+        {
+            // Set số báo giá cũ cho form mới
+            if (string.IsNullOrEmpty(e.Row["SoBGCopy"].ToString()) && !string.IsNullOrEmpty(oldBG))
+            {
+                e.Row["SoBGCopy"] = oldBG;
+                oldBG = "";
+            }
+        }
+        private void BaoGia_TableNewRow(object sender, DataTableNewRowEventArgs e)
+        {
+            CDTForm frm = (_data.FrmMain as CDTForm);
+            if (frm.FrmDesigner.formAction == FormAction.Copy)
+            {
+                //Luu so Bao gia cu.
+                DataRowView drMaster = _data.BsMain.Current as DataRowView;
+                oldBG = drMaster.Row["SoBG"].ToString();
+            }
+            else
+            {
+                oldBG = "";
+            }
+        }
+
+        public DataCustomFormControl Data
+        {
+            set { _data = value; }
+        }
+
+        public InfoCustomControl Info
+        {
+            get { return _info; }
+        }
+    }
+}
