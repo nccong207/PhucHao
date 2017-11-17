c3bef7331f6fa7398f0046badced69e7670190e4
linh0893 <linh0893@gmail.com>
2017-11-17 21:50:08 +0700

    to mau phieu de nghi va phieu mua hang

---
+++ ToMauBT/ToMauBT.cs
@@ -0,0 +1,46 @@
+﻿using DevExpress.XtraGrid;
+using DevExpress.XtraGrid.Views.Grid;
+using Plugins;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ToMauBT
+{
+    public class ToMauBT : ICControl
+    {
+        private DataCustomFormControl _data;
+        private InfoCustomControl _info = new InfoCustomControl(IDataType.MasterDetail);
+      
+
+        public void AddEvent()
+        {
+            var tb = _data.DrTableMaster["TableName"].ToString();
+            if (tb != "MTDeNghi" && tb != "MTMuaHang" )
+                return;
+
+            var gvMain = (_data.FrmMain.Controls.Find("gcMain", true)[0] as GridControl).MainView as GridView;
+            gvMain.RowStyle += GvMain_RowStyle;
+        }
+
+        private void GvMain_RowStyle(object sender, RowStyleEventArgs e)
+        {
+            GridView View = sender as GridView;
+            if (e.RowHandle >= 0)
+            {
+                bool duyet = (bool) View.GetRowCellValue(e.RowHandle, View.Columns["Duyet"]);
+
+                if (duyet)
+                {
+                    //green
+                    e.Appearance.BackColor = Color.LightGreen;
+                    e.Appearance.BackColor2 = Color.LightGreen;
+                }
+            }
+        }
+
+        InfoCustomControl ICControl.Info {get { return _info; } }
+        DataCustomFormControl ICControl.Data { set { _data = value; } }
+    }
+}
