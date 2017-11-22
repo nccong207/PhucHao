84e5203676e538c83334a0008ee35c7d5fd67485
linh0893 <linh0893@gmail.com>
2017-11-22 21:02:02 +0700

    update xu ly to mau cho phieu de nghi thanh toan và phieu de nghi mua hang

--- ToMauBT/ToMauBT.cs
+++ ToMauBT/ToMauBT.cs
@@ -3,6 +3,7 @@ using DevExpress.XtraGrid.Views.Grid;
 using Plugins;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Drawing;
 using System.Text;
 
@@ -12,34 +13,51 @@ namespace ToMauBT
     {
         private DataCustomFormControl _data;
         private InfoCustomControl _info = new InfoCustomControl(IDataType.MasterDetail);
-      
+        GridView gvMain;
 
         public void AddEvent()
         {
             var tb = _data.DrTableMaster["TableName"].ToString();
-            if (tb != "MTDeNghi" && tb != "MTMuaHang" )
+            if (tb != "MTDeNghi" && tb != "MTDNTT" )
                 return;
 
-            var gvMain = (_data.FrmMain.Controls.Find("gcMain", true)[0] as GridControl).MainView as GridView;
-            gvMain.RowStyle += GvMain_RowStyle;
+            gvMain = (_data.FrmMain.Controls.Find("gcMain", true)[0] as GridControl).MainView as GridView;
+            //gvMain.RowStyle += GvMain_RowStyle;
+            _data.FrmMain.Load += new EventHandler(FrmMain_Load);
         }
 
-        private void GvMain_RowStyle(object sender, RowStyleEventArgs e)
-        {
-            GridView View = sender as GridView;
-            if (e.RowHandle >= 0)
-            {
-                bool duyet = (bool) View.GetRowCellValue(e.RowHandle, View.Columns["Duyet"]);
+        //private void GvMain_RowStyle(object sender, RowStyleEventArgs e)
+        //{
+        //    GridView View = sender as GridView;
+        //    if (e.RowHandle >= 0)
+        //    {
+        //        bool duyet = (bool) View.GetRowCellValue(e.RowHandle, View.Columns["Duyet"]);
 
-                if (duyet)
-                {
-                    //green
-                    e.Appearance.BackColor = Color.LightGreen;
-                    e.Appearance.BackColor2 = Color.LightGreen;
-                }
+        //        if (duyet)
+        //        {
+        //            //green
+        //            e.Appearance.BackColor = Color.LightGreen;
+        //            e.Appearance.BackColor2 = Color.LightGreen;
+        //        }
+        //    }
+        //}
+
+        void FrmMain_Load(object sender, EventArgs e)
+        {
+            if ((_data.BsMain.DataSource as DataSet).Tables[0].Columns.Contains("Duyet"))
+            {
+                //dung class StyleFormatCondition cho Duyet
+                StyleFormatCondition d = new StyleFormatCondition();
+                gvMain.FormatConditions.Add(d);
+                //thiet lap dieu kien
+                d.Column = gvMain.Columns["Duyet"];
+                d.Condition = FormatConditionEnum.Equal;
+                d.Value1 = true;
+                //thiet lap dinh dang
+                d.Appearance.BackColor = Color.LightGreen;
+                d.ApplyToRow = true;
             }
         }
-
         InfoCustomControl ICControl.Info {get { return _info; } }
         DataCustomFormControl ICControl.Data { set { _data = value; } }
     }
