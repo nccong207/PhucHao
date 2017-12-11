ad502384950982f08a2ae3fe9290b25c98e42164
linh0893 <linh0893@gmail.com>
2017-12-11 21:45:20 +0700

    update new form lay danh sach khuyen mai nhieu sp

---
+++ LayDuLieuKhuyenMai/DsSanPham.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace LayDuLieuKhuyenMai
+{
+    partial class DsSanPham
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraGrid.GridLevelNode gridLevelNode1 = new DevExpress.XtraGrid.GridLevelNode();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.MaSP = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.rItemLookUpEditMaSP = new DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit();
+            this.SLTang = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.btnSave = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rItemLookUpEditMaSP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.EmbeddedNavigator.Name = "";
+            gridLevelNode1.RelationName = "Level1";
+            this.gridControl1.LevelTree.Nodes.AddRange(new DevExpress.XtraGrid.GridLevelNode[] {
+            gridLevelNode1});
+            this.gridControl1.Location = new System.Drawing.Point(5, 5);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
+            this.rItemLookUpEditMaSP});
+            this.gridControl1.Size = new System.Drawing.Size(391, 227);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.MaSP,
+            this.SLTang});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // MaSP
+            // 
+            this.MaSP.Caption = "Mã sản phẩm";
+            this.MaSP.ColumnEdit = this.rItemLookUpEditMaSP;
+            this.MaSP.FieldName = "MaSPTang";
+            this.MaSP.Name = "MaSP";
+            this.MaSP.Visible = true;
+            this.MaSP.VisibleIndex = 0;
+            this.MaSP.Width = 236;
+            // 
+            // rItemLookUpEditMaSP
+            // 
+            this.rItemLookUpEditMaSP.AllowNullInput = DevExpress.Utils.DefaultBoolean.False;
+            this.rItemLookUpEditMaSP.AutoHeight = false;
+            this.rItemLookUpEditMaSP.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.rItemLookUpEditMaSP.Name = "rItemLookUpEditMaSP";
+            this.rItemLookUpEditMaSP.NullText = "";
+            // 
+            // SLTang
+            // 
+            this.SLTang.Caption = "Số lượng tặng";
+            this.SLTang.DisplayFormat.FormatString = "###,###,###0";
+            this.SLTang.DisplayFormat.FormatType = DevExpress.Utils.FormatType.Numeric;
+            this.SLTang.FieldName = "SLTang";
+            this.SLTang.Name = "SLTang";
+            this.SLTang.Visible = true;
+            this.SLTang.VisibleIndex = 1;
+            this.SLTang.Width = 134;
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.simpleButton2);
+            this.panelControl1.Controls.Add(this.btnSave);
+            this.panelControl1.Controls.Add(this.gridControl1);
+            this.panelControl1.Location = new System.Drawing.Point(1, -1);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(403, 272);
+            this.panelControl1.TabIndex = 1;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(30, 239);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Lưu";
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(271, 239);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton2.TabIndex = 2;
+            this.simpleButton2.Text = "Hủy";
+            // 
+            // DsSanPham
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 273);
+            this.Controls.Add(this.panelControl1);
+            this.Name = "DsSanPham";
+            this.Text = "Nhập sản phẩm khuyến mãi";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.DsSanPham_FormClosed);
+            this.Load += new System.EventHandler(this.DsSanPham_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rItemLookUpEditMaSP)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn MaSP;
+        private DevExpress.XtraGrid.Columns.GridColumn SLTang;
+        private DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit rItemLookUpEditMaSP;
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.SimpleButton btnSave;
+    }
+}
---
+++ LayDuLieuKhuyenMai/DsSanPham.cs
@@ -0,0 +1,50 @@
+﻿using CDTDatabase;
+using DevExpress.XtraEditors;
+using DevExpress.XtraEditors.Repository;
+using DevExpress.XtraGrid.Views.Grid;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LayDuLieuKhuyenMai
+{
+    public partial class DsSanPham : XtraForm
+    {
+        Database db = Database.NewDataDatabase();
+        public BindingSource source = new BindingSource();
+        public DsSanPham()
+        {
+            InitializeComponent();
+            GetDataSP();
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            
+        }
+
+        private void GetDataSP()
+        {
+            string sql = string.Format(@"SELECT MaSP, TenSP FROM mDMSP");
+            DataTable dt = db.GetDataTable(sql);
+
+            rItemLookUpEditMaSP.DataSource = dt;
+            rItemLookUpEditMaSP.DisplayMember = "TenSP";
+            rItemLookUpEditMaSP.ValueMember = "MaSP";
+        }
+
+        private void DsSanPham_Load(object sender, EventArgs e)
+        {
+            gridControl1.DataSource = source;
+            (gridControl1.MainView as GridView).BestFitColumns();
+            (gridControl1.MainView as GridView).AddNewRow();
+        }
+
+        private void DsSanPham_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            gridControl1.RefreshDataSource();
+        }
+    }
+}
