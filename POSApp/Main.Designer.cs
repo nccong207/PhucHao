0e43404b472fa3a47f9a2f3dfaf3522dd3715447
linh0893 <linh0893@gmail.com>
2018-02-07 21:36:22 +0700

    update nhap macuon

--- POSApp/Main.Designer.cs
+++ POSApp/Main.Designer.cs
@@ -29,27 +29,110 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.MaCuon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.KyHieu = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Kho = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SoTon = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(296, 38);
+            this.textBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(202, 119);
+            this.label1.Location = new System.Drawing.Point(13, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(117, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Đăng nhập thành công";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã cuộn";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::POSApp.Properties.Resources._256_256_583f916fff606f7649d9859ca5097961;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(328, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(53, 38);
+            this.button1.TabIndex = 2;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaCuon,
+            this.KyHieu,
+            this.Kho,
+            this.SoTon});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(631, 284);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // MaCuon
+            // 
+            this.MaCuon.HeaderText = "Mã cuộn";
+            this.MaCuon.Name = "MaCuon";
+            this.MaCuon.ReadOnly = true;
+            // 
+            // KyHieu
+            // 
+            this.KyHieu.HeaderText = "Ký hiệu";
+            this.KyHieu.Name = "KyHieu";
+            this.KyHieu.ReadOnly = true;
+            // 
+            // Kho
+            // 
+            this.Kho.HeaderText = "Khổ";
+            this.Kho.Name = "Kho";
+            this.Kho.ReadOnly = true;
+            // 
+            // SoTon
+            // 
+            this.SoTon.HeaderText = "Số tồn";
+            this.SoTon.Name = "SoTon";
+            this.SoTon.ReadOnly = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(12, 93);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(637, 290);
+            this.panel1.TabIndex = 4;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(531, 271);
+            this.ClientSize = new System.Drawing.Size(661, 395);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.Text = "Phần mềm quản lý giấy cuộn tại xưởng";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +140,14 @@
 
         #endregion
 
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaCuon;
+        private System.Windows.Forms.DataGridViewTextBoxColumn KyHieu;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Kho;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SoTon;
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- POSApp/Main.cs
+++ POSApp/Main.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using CDTDatabase;
+using CDTLib;
+using DevExpress.XtraEditors;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
 
@@ -10,9 +14,55 @@ namespace POSApp
 {
     public partial class Main : Form
     {
+        AppCon ac = new AppCon();
         public Main(DataRow drUser)
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string StructConnection = ac.GetValue("StructDb");
+            if (string.IsNullOrEmpty(StructConnection))
+            {
+                XtraMessageBox.Show("Không tìm thấy chuỗi kết nối database", Config.GetValue("PackageName").ToString());
+                this.Close();
+            }
+            StructConnection = Security.DeCode(StructConnection);
+            StructConnection = StructConnection.Replace("POS", "HTCPH");
+
+            Database db = Database.NewCustomDatabase(StructConnection);
+            var macuon = textBox1.Text;
+            var soTon = db.GetValue(string.Format("SELECT SoLuong FROM TonKhoNL WHERE MaCuon = '{0}'", macuon.Trim()));
+            decimal soluongTon = 0;
+            if (soTon != null)
+            {
+                soluongTon = Convert.ToDecimal(soTon.ToString());
+            }
+
+            var manl = db.GetValue(string.Format("SELECT MaNL FROM DT42 WHERE MaCuon = '{0}'", macuon.Trim()));
+            string kyhieu = "", kho = "";
+            if (manl != null)
+            {
+                DataTable dmNL = db.GetDataTable(string.Format("SELECT KyHieu, Kho FROM wDMNL2 WHERE MaNL = '{0}'", manl.ToString()));
+                if (dmNL.Rows.Count > 0)
+                {
+                    kyhieu = dmNL.Rows[0]["KyHieu"].ToString();
+                    kho = dmNL.Rows[0]["Kho"].ToString();
+                }
+            }
+
+            //DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+            //row.Cells[0].Value = macuon;
+            //row.Cells[1].Value = kyhieu;
+            //row.Cells[2].Value = kho;
+            //row.Cells[3].Value = soluongTon;
+            dataGridView1.Rows.Add(macuon, kyhieu, kho, soluongTon);
+        }
+
+        private void AddToGrid()
+        {
+
         }
     }
 }
